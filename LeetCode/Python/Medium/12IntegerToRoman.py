bef5536d6ac0d6419945ee0d3679ae6489c59c8a
rbarnes123 <rbarnes1@umbc.edu>
2025-09-14 12:50:16 -0400

    Refactored IntToRoman
    Changed approach to greedy algorithm and stored values for better readability/access

--- LeetCode/Python/Medium/12IntegerToRoman.py
+++ LeetCode/Python/Medium/12IntegerToRoman.py
@@ -1,50 +1,14 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        roman = ""
-        while num != 0:
-            numLen = len(str(num))
-            print(num)
-            if numLen >= 4:
-                roman += 'M'
-                num -= 1000
-            elif numLen == 3:
-                if num >= 900:
-                    roman += "CM"
-                    num -= 900
-                elif num >= 500:
-                    roman += 'D'
-                    num -= 500
-                elif num >= 400:
-                    roman += "CD"
-                    num -= 400
-                elif num >= 100:
-                    roman +='C'
-                    num -= 100
-            elif numLen == 2:
-                if num >= 90:
-                    roman += "XC"
-                    num-= 90
-                elif num >= 50:
-                    roman += 'L'
-                    num -= 50
-                elif num >= 40:
-                    roman += "XL"
-                    num -= 40
-                elif num >= 10:
-                    roman += 'X'
-                    num -= 10
-            else:
-                if num == 9:
-                    roman += "IX"
-                    num -= 9
-                elif num >= 5:
-                    roman += 'V'
-                    num -= 5
-                elif num >= 4:
-                    roman += "IV"
-                    num -= 4
-                else:
-                    roman += 'I'
-                    num -= 1
-        return roman
+        roman = []
+        numerals = { 1000:'M', 900:"CM", 500:"D", 400:"CD" , 100:"C",
+        90:"XC",50:'L',40:"XL",10:'X',9:"IX",5:'V',4:"IV",1:'I'}
+        while num > 0:
+            for number in numerals.keys():
+                if num >= number:
+                    roman += numerals[number]
+                    num -= number
+                    break
+        return "".join(roman)
+            
             
