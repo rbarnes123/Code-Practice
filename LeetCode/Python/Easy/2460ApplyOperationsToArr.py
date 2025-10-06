f5984e316ff2722fa4e7c67769b926132f551c34
rbarnes123 <rbarnes1@umbc.edu>
2025-10-06 11:37:38 -0400

    More python Practice
    Practiced more two pointer and using array functions

---
+++ LeetCode/Python/Easy/2460ApplyOperationsToArr.py
@@ -0,0 +1,14 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)- 1):
+            if nums[i] == nums[i+1]:
+                nums[i] *= 2
+                nums[i+1] = 0
+        
+        write = 0
+        for read in range(len(nums)):
+            if nums[read] != 0:
+                nums[write], nums[read] = nums[read], nums[write]
+                write += 1    
+        return nums
+        
